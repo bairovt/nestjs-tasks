70bcd0aa4aadc6ff591df850a9edf67aea66d464
Tumen Bairov <bairovt@gmail.com>
2020-03-16 08:47:21 +0900

    jwt passport modules added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,18 @@ import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule, AuthModule],
+  imports: [
+    JwtModule.register({
+      secret: 'someSuperSecretKeyToSignJwt-slksmlvkcm',
+      signOptions: {
+        expiresIn: 3600 // 1 hour
+      }
+    }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    TypeOrmModule.forRoot(typeOrmConfig), TasksModule, AuthModule],
 })
-export class AppModule {}
+export class AppModule { }
