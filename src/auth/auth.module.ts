ea8d72e1909b63727b3277933bc491a2a5e6c410
Tumen Bairov <bairovt@gmail.com>
2020-03-24 23:18:24 +0900

    temp

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,18 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepo } from './repos/user.repo';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepo])],
+  imports: [
+    JwtModule.register({
+      secret: 'some-very-strong key-kjalvmalj4309m',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    TypeOrmModule.forFeature([UserRepo]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@nestjs/common';
-import { UserRepo } from './repos/user.repo';
 import { InjectRepository } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
+import { UserRepo } from './repos/user.repo';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
-import { User } from './entities/user.entity';
+import { JwtPaylod as JwtPayload } from '../../dist/auth/interfaces/jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepo)
     private userRepo: UserRepo,
-  ) { }
+    private jwtService: JwtService,
+  ) {}
 
   signUp(authCredentials: AuthCredentialsDto): Promise<void> {
     return this.userRepo.signUp(authCredentials);
   }
 
-  async signIn(authCredentials: AuthCredentialsDto): Promise<string> {
-    return this.userRepo.signIn(authCredentials);
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    this.userRepo.signIn(authCredentialsDto);
+    const payload: JwtPayload = {
+      username: authCredentialsDto.username,
+    };
+    const jwtToken = this.jwtService.sign(payload);
+    return jwtToken;
   }
 }
